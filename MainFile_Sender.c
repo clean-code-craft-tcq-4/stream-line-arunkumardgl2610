242ff80e7a008ab4ede0ca6a044807664101d272
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-09-14 14:52:33 +0530

    Update MainFile_Sender.c

--- MainFile_Sender.c
+++ MainFile_Sender.c
@@ -9,7 +9,7 @@ int main()
   {
     Battery_Health_sensor1[SOC] = Sensor1DataCreateAlgorithm(SOC);
     Battery_Voltage_sensor2[SOC] = Sensor2DataCreateAlgorithm(SOC);
-    printf("Battery_Health @SOC_%d% = %d\n", SOC, Sensor1_Data[SOC]);
-    printf("Battery_Voltage @SOC_%d% = %f\n", SOC, Sensor2_Data[SOC]);
+    printf("Battery_Health @SOC_%d% = %d\n", SOC, Battery_Health_sensor1[SOC]);
+    printf("Battery_Voltage @SOC_%d% = %f\n", SOC, Battery_Voltage_sensor2[SOC]);
   }
 }
