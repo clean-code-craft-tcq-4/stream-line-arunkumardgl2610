d4af0be6f3aa0fb91760d58233ddceb910ee17fd
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-09-14 13:42:18 +0530

    Update DataFromSensor.h

--- DataFromSensor.h
+++ DataFromSensor.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 float Battery_Internal_Resistance;
 int Battery_Health=0,
-int Sensor1DataAlgorithm(int Battery_Capacity)
+int Sensor1DataAlgorithm(float Battery_Capacity)
 {
   Battery_Internal_Resistance = (Battery_Capacity/5);
   Battery_Health = (Battery_Internal_Resistance*10);
