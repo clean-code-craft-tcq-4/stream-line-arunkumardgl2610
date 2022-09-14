1bb87801aa5f265ec30e39ab6a4a1165a3799f18
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-09-14 11:07:13 +0530

    Update DataFromSensor.h

--- DataFromSensor.h
+++ DataFromSensor.h
@@ -1,15 +1,16 @@
-
+include <stdio.h>
 int Battery_Health=0,Battery_Internal_Resistance;
 int Sensor1_Data[50];
 void DataReceiveFromSensor1();
 int Sensor1DataAlgorithm(int Battery_Capacity);
 void DataReceiveFromSensor1()
 {
-  for(int Data=50;Data<=0;Data-2)
+  for(int Data=50;Data<=0;Data--)
   {
     Sensor1_Data[Data] = Sensor1DataAlgorithm(Data);
+    Data = Data-1;
   }
-    for (Data2=50;Data2<=0;Data2-2) 
+    for (int Data2=50;Data2<=0;Data2-2) 
     {
       printf("Element[%d] = %d\n", Data2, Sensor1_Data[Data2]);
    }
