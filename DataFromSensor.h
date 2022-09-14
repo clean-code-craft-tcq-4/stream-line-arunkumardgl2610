50df51cff3f303bca74386035d4a049379372667
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-09-14 13:52:44 +0530

    Update DataFromSensor.h

--- DataFromSensor.h
+++ DataFromSensor.h
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #define Battery_Internal_Resistance 10
-int Battery_Internal_Resistance;
 int Battery_Health=0;
 int Sensor1DataAlgorithm(int Battery_Capacity)
 {
-  Battery_Health = (Battery_Capacity*Battery_Internal_Resistance/5);
-  return Battery_Health;
+  Battery_Health = (Battery_Capacity*Battery_Internal_Resistance/5); 
 }
