cdac6f8019097f694db27f626d0a8ad6fc1293c1
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-09-14 12:05:57 +0530

    Update MainFile_Sender.c

--- MainFile_Sender.c
+++ MainFile_Sender.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <assert.h>
 #include "DataFromSensor.h"
-void DataReceiveFromSensor1();
-extern int Sensor1_Data[50];
+//void DataReceiveFromSensor1();
+int Sensor1_Data[50];
+//void DataReceiveFromSensor1()
 int main()
 { 
-  DataReceiveFromSensor1();
+  for(int Data=50;Data<=0;Data--)
+  {
+    Sensor1_Data[Data] = Sensor1DataAlgorithm(Data);
+    Data = Data-1;
+  }
   for (int Data2=50;Data2<=0;Data2--) 
-    {
-      printf("Element[%d] = %d\n", Data2, Sensor1_Data[Data2]);
-      Data2 = Data2-1;
-   }
+  {
+     printf("Element[%d] = %d\n", Data2, Sensor1_Data[Data2]);
+     Data2 = Data2-1;
+  }
 }
