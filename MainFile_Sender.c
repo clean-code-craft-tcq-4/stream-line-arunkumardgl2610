ea851da596e82d808797492b740fb60431bf2558
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-09-14 14:20:40 +0530

    Update MainFile_Sender.c

--- MainFile_Sender.c
+++ MainFile_Sender.c
@@ -2,14 +2,20 @@
 #include <assert.h>
 #include "DataFromSensor.h"
 int Sensor1_Data[50];
+flaot Sensor2_Data[50];
 int main()
 { 
   for(int Data=50;Data>=0;Data--)
   {
     Sensor1_Data[Data] = Sensor1DataAlgorithm(Data);
   }
+  for(int Data4=100;Data4>=50;Data4--)
+  {
+    Sensor2_Data[Data4] = Sensor2DataAlgorithm(Data4);
+  }
   for (int Data2=50;Data2>=0;Data2--) 
   {
     printf("Element[%d] = %d\n", Data2, Sensor1_Data[Data2]);
+    printf("Element[%d] = %f\n", Data2, Sensor2_Data[Data2]);
   }
 }
