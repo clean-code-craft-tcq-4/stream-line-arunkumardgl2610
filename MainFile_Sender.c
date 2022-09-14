229e665ab8e0922c53f1523daf15c4a9ca5ad35d
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-09-14 12:18:17 +0530

    Update MainFile_Sender.c

--- MainFile_Sender.c
+++ MainFile_Sender.c
@@ -4,15 +4,14 @@
 int Sensor1_Data[50];
 int main()
 { 
-  for(int Data=50;Data<=0;Data--)
+  for(int Data=50;Data>=0;Data--)
   {
     Sensor1_Data[Data] = Sensor1DataAlgorithm(Data);
     Data = Data-1;
   }
   for (int Data2=50;Data2>=0;Data2--) 
   {
-    printf("Element = %d\n",Data2);
- //    printf("Element[%d] = %d\n", Data2, Sensor1_Data[Data2]);
-  //   Data2 = Data2-1;
+    printf("Element[%d] = %d\n", Data2, Sensor1_Data[Data2]);
+     Data2 = Data2-1;
   }
 }
